36c25c7f2a94370810a9b18ff4b81eccfce35b6b
ElP1raguas <ruben.martinez.lopez@udc.es>
2026-02-28 14:12:28 +0100

    Checkpoint

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .database import Base
 
 class UserDB(Base):
@@ -6,11 +6,11 @@ class UserDB(Base):
     email = Column(String, primary_key=True, index=True)
     client_salt = Column(String, nullable=False)
     mah_hash = Column(String, nullable=False)
-    encrypted_vk = Column(Text, nullable=False)
+    encrypted_vk = Column(String, nullable=False)
 
 class VaultItemDB(Base):
     __tablename__ = "vault_items"
     id = Column(Integer, primary_key=True, index=True)
     user_email = Column(String, ForeignKey("users.email"))
-    encrypted_payload = Column(Text, nullable=False)
+    encrypted_payload = Column(String, nullable=False)
     nonce = Column(String, nullable=False)
